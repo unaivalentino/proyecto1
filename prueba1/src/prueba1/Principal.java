e9c483f9f97ec736892c8bb77d70edaa65c3f8b0
unai <unaivn@gmail.com>
2022-01-24 08:58:13 +0100

    cambioPrincipal

--- prueba1/src/prueba1/Principal.java
+++ prueba1/src/prueba1/Principal.java
@@ -4,6 +4,9 @@ public class Principal {
 	
 	public static void main(String[] args) {
 		System.out.println("Hola mundo java-git");
+		System.out.println("Hola mundo java-git");
+		System.out.println("Adios");
+		System.out.println("Pruebas Git. Se añade Cambio1");
 
 	}
 
